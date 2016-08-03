f92b06bdd0aa76ff23b2263a6f64917b1b24012d
Gitju <gitju@cryptocurrency.science>
2016-08-03 18:26:15 +0200

    Upstream update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var packageObject = require('./package.json');
+
 module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
@@ -5,7 +7,7 @@ module.exports = function (grunt) {
 		combine: {
 			single: {
 				input: "./src/bitaddress-ui.html",
-				output: "./index.html",
+				output: "./paper.machinecoin.org.html",
 				tokens: [
 					{ token: "//array.map.js", file: "./src/array.map.js" },
 					{ token: "//biginteger.js", file: "./src/biginteger.js" },
@@ -24,9 +26,12 @@ module.exports = function (grunt) {
 					{ token: "//cryptojs.ripemd160.js", file: "./src/cryptojs.ripemd160.js" },
 					{ token: "//crypto-scrypt.js", file: "./src/crypto-scrypt.js" },
 					{ token: "//ellipticcurve.js", file: "./src/ellipticcurve.js" },
+					{ token: "//secrets.js", file: "./src/secrets.js" },
 					{ token: "//ninja.key.js", file: "./src/ninja.key.js" },
 					{ token: "//ninja.misc.js", file: "./src/ninja.misc.js" },
 					{ token: "//ninja.onload.js", file: "./src/ninja.onload.js" },
+					{ token: "//ninja.qrcode.js", file: "./src/ninja.qrcode.js" },
+					{ token: "//ninja.seeder.js", file: "./src/ninja.seeder.js" },
 					{ token: "//ninja.unittests.js", file: "./src/ninja.unittests.js" },
 					{ token: "//ninja.translator.js", file: "./src/ninja.translator.js" },
 					{ token: "//ninja.singlewallet.js", file: "./src/ninja.singlewallet.js" },
@@ -34,16 +39,42 @@ module.exports = function (grunt) {
 					{ token: "//ninja.bulkwallet.js", file: "./src/ninja.bulkwallet.js" },
 					{ token: "//ninja.brainwallet.js", file: "./src/ninja.brainwallet.js" },
 					{ token: "//ninja.vanitywallet.js", file: "./src/ninja.vanitywallet.js" },
+					{ token: "//ninja.splitwallet.js", file: "./src/ninja.splitwallet.js" },
 					{ token: "//ninja.detailwallet.js", file: "./src/ninja.detailwallet.js" },
 					{ token: "//qrcode.js", file: "./src/qrcode.js" },
 					{ token: "//securerandom.js", file: "./src/securerandom.js" },
-					{ token: "//main.css", file: "./src/main.css" }
+					{ token: "//main.css", file: "./src/main.css" },
+					{ token: "//version", string: packageObject.version },
+					// cultures
+					{ token: "//cs.js", file: "./src/culture/cs.js" },
+					{ token: "//de.js", file: "./src/culture/de.js" },
+					{ token: "//el.js", file: "./src/culture/el.js" },
+					{ token: "//es.js", file: "./src/culture/es.js" },
+					{ token: "//fr.js", file: "./src/culture/fr.js" },
+					{ token: "//hu.js", file: "./src/culture/hu.js" },
+					{ token: "//it.js", file: "./src/culture/it.js" },
+					{ token: "//jp.js", file: "./src/culture/jp.js" },
+					{ token: "//pt-br.js", file: "./src/culture/pt-br.js" },
+					{ token: "//ru.js", file: "./src/culture/ru.js" },
+					{ token: "//zh-cn.js", file: "./src/culture/zh-cn.js" }
 				]
+			}
+		},
+		
+		lineending: {               // Task
+			dist: {                   // Target
+				options: {              // Target options
+					eol: 'lf'
+				},
+				files: {                // Files to process
+					'./paper.machinecoin.org.html': ['./paper.machinecoin.org.html']
+				}
 			}
 		}
 	});
 
 	grunt.file.defaultEncoding = 'utf-8';
 	grunt.loadNpmTasks("grunt-combine");
-	grunt.registerTask("default", ["combine:single"]);
+	grunt.loadNpmTasks('grunt-lineending');
+	grunt.registerTask("default", ["combine:single", "lineending"]);
 };
